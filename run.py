ef4176da20635ee21bde0c10333ce79705cffd00
Abhishek Rajput <abhishek620380@gmail.com>
2024-06-01 12:23:48 +0530

    Added Https Support ( Dirty Self Signed Certificate )

--- run.py
+++ run.py
@@ -3,5 +3,6 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True , port=8000 , host="0.0.0.0")
-
+    context  = ("cert.pem" , "key.pem")
+    app.run(debug=True , port=8000 , host="0.0.0.0" , ssl_context=context)
+    
